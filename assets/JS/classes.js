3d795a5a82c9f013eacf043cecef58297070ca27
Augusto Siqueira <augusto.sqra@outlook.com>
2023-05-18 21:03:47 -0300

    Criado um novo arquivo JS, chamado classes, para nele criar as classes dos personagens, e no arquivo script.js somente executar ações. Foram criados os personagens e a arena de luta, também foi adicionado a ação de ataque ao  (attackButton).

---
+++ assets/JS/classes.js
@@ -0,0 +1,108 @@
+// Base de criação dos personagens
+class Character {
+    _life = 1;
+    maxlife = 1;
+    attack = 0;
+    defense = 0;
+
+    constructor (name) {
+        this.name = name;
+    }
+
+    get life() {
+            return this._life;
+    }
+    set life(newLife) {
+        this._life = newLife < 0 ? 0 : newLife;
+    }
+}
+
+// Criando os personagens com suas características, (extends) é usado para vincular os dados da base (Character) o personagem que esta sendo criado
+// O Super vai acessar o contrutor que estamos extendendo, no caso o (Character).
+
+//GUERREIRO
+class Knight extends Character {
+    constructor (name) {
+            super(name);
+            this.life = 100;
+            this.attack = 10;
+            this.defense = 8;
+            this.maxlife = this.life;
+    }
+}
+
+//MAGO
+class Sorcerer extends Character {
+    constructor (name) {
+            super(name);
+            this.life = 80;
+            this.attack = 15;
+            this.defense = 3;
+            this.maxlife = this.life;
+    }
+}
+
+//LittleMonster
+class LittleMonster extends Character {
+    constructor () {
+        super('LittleMonster');
+                this.life = 40;
+                this.attack = 4;
+                this.defense = 4;
+                this.maxlife = this.life;
+    }
+}
+
+//BigMonster
+class BigMonster extends Character {
+    constructor () {
+        super('BigMonster');
+                this.life = 120;
+                this.attack = 16;
+                this.defense = 6;
+                this.maxlife = this.life;
+    }
+}
+
+//Criando o cenário da luta, onde deve ser inserido quem são os dois lutadores e os elementos de cada um deles.
+class Stage {
+    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+    }
+
+    //criando a função que irá dar o start no jogo
+    start() {
+        this.update();
+    
+        // Criando ação de clique para atacar
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+    }
+
+    //Criando a função (update) que será responsável por atualizar a tela com as informações dos lutadores
+    update() {
+        //fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+        //preenchimento da barra de vida
+        let f1pct = (this.fighter1.life / this.fighter1.maxlife) * 100;
+        //Apresentação gráfica da quantidade de vida de 0 à 100
+        this.fighter1El.querySelector('.bar').style.width = `${f1pct}%`;
+
+
+        //fighter 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        //preenchimento da barra de vida
+        let f2pct = (this.fighter2.life / this.fighter2.maxlife) * 100;
+        //Apresentação gráfica da quantidade de vida de 0 à 100
+        this.fighter2El.querySelector('.bar').style.width = `${f2pct}%`;
+    }
+    
+    doAttack(attacking, attacked) {
+        console.log(`${attacking.name} está atacando ${attacked.name}`);
+
+        this.update();
+    }
+}
