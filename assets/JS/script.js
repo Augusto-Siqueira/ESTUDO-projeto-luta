c3ad2131099c86a695c02532bdc3ec85a1f2238b
Augusto Siqueira <augusto.sqra@outlook.com>
2023-05-23 18:48:37 -0300

    Realizado a criação da classe para exibir os resultados em tela

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,5 @@
+let log = new Log(document.querySelector('.log'));
+
 let char = new Knight('Zabela')
 let monster = new LittleMonster()
 
@@ -5,10 +7,11 @@ const stage = new Stage(
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log,
 );
 
 stage.start();
 
 
-console.log(`${attacking.name} está atacando ${attacked.name}`);
+//console.log(`${attacking.name} está atacando ${attacked.name}`);
